e76be60fe1d80d68e0965365225807fd90949049
Eyal Amir <eyalamirmusic@gmail.com>
2020-02-09 10:51:15 +0200

    Improved JUCE helpers to also store the Projucer path

--- SDKs/Cmake/JUCEHelpers.cmake
+++ SDKs/Cmake/JUCEHelpers.cmake
@@ -2,10 +2,8 @@ cmake_minimum_required(VERSION 3.4)
 
 include(GitHelpers)
 
-set (projucerExeFile "Before")
-
-function(build_projucer)
-    set (projucerDir "${PROJECT_SOURCE_DIR}/JUCE/extras/Projucer/Builds")
+function(build_projucer_func projucerEXE)
+    set(projucerDir "${PROJECT_SOURCE_DIR}/JUCE/extras/Projucer/Builds")
 
     if (APPLE)
         set(projucerBuildDir "${projucerDir}/MacOSX")
@@ -13,18 +11,16 @@ function(build_projucer)
         execute_process(COMMAND "xcodebuild" "-configuration" "Release"
                 WORKING_DIRECTORY ${projucerBuildDir})
 
-        message ("${ProjucerExeFile}")
-        set (projucerExeFile "${projucerBuildDir}/build/Release/Projucer.app")
-        message ("${ProjucerExeFile}")
+        set(${projucerEXE} "${projucerBuildDir}/build/Release/Projucer.app" PARENT_SCOPE)
     else ()
         set(projucerBuildDir "${projucerDir}/VisualStudio2019")
         message("Remember to set up the build for non-apple systems")
     endif ()
 endfunction()
 
-function(update_juce)
+macro(update_juce)
     set(juceGit "https://github.com/WeAreROLI/JUCE.git")
     update_git(${juceGit} "JUCE")
-    build_projucer()
-endfunction()
+    build_projucer_func(projucerEXE)
+endmacro()
 
