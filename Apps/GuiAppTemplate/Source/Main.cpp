36a36b9dfd4a91167dcc697949881423472c401e
Eyal Amir <eyalamirmusic@gmail.com>
2020-02-19 23:37:34 +0200

    Improved default gui app template

--- Apps/GuiAppTemplate/Source/Main.cpp
+++ Apps/GuiAppTemplate/Source/Main.cpp
@@ -1,78 +1,29 @@
-#include <JuceHeader.h>
-#include "MainComponent.h"
+#include "MainWindow.h"
 
-class GuiAppTemplateApplication  : public JUCEApplication
+class GuiAppTemplateApplication : public JUCEApplication
 {
 public:
-    GuiAppTemplateApplication() {}
-
-    const String getApplicationName() override       { return ProjectInfo::projectName; }
-    const String getApplicationVersion() override    { return ProjectInfo::versionString; }
-    bool moreThanOneInstanceAllowed() override       { return true; }
-
-    void initialise (const String& commandLine) override
+    const String getApplicationName() override { return ProjectInfo::projectName; }
+    const String getApplicationVersion() override
     {
-        mainWindow.reset (new MainWindow (getApplicationName()));
+        return ProjectInfo::versionString;
+    }
+    bool moreThanOneInstanceAllowed() override { return true; }
+
+    void initialise(const String& commandLine) override
+    {
+        mainWindow = std::make_unique<MainWindow>(getApplicationName());
     }
 
-    void shutdown() override
-    {
-        mainWindow = nullptr;
-    }
+    void shutdown() override { mainWindow.reset(); }
 
-    void systemRequestedQuit() override
-    {
-        quit();
-    }
+    void systemRequestedQuit() override { quit(); }
 
-    void anotherInstanceStarted (const String& commandLine) override
-    {
-    }
-
-    class MainWindow    : public DocumentWindow
-    {
-    public:
-        MainWindow (String name)  : DocumentWindow (name,
-                                                    Desktop::getInstance().getDefaultLookAndFeel()
-                                                                          .findColour (ResizableWindow::backgroundColourId),
-                                                    DocumentWindow::allButtons)
-        {
-            setUsingNativeTitleBar (true);
-            setContentOwned (new MainComponent(), true);
-
-           #if JUCE_IOS || JUCE_ANDROID
-            setFullScreen (true);
-           #else
-            setResizable (true, true);
-            centreWithSize (getWidth(), getHeight());
-           #endif
-
-            setVisible (true);
-        }
-
-        void closeButtonPressed() override
-        {
-            // This is called when the user tries to close this window. Here, we'll just
-            // ask the app to quit when this happens, but you can change this to do
-            // whatever you need.
-            JUCEApplication::getInstance()->systemRequestedQuit();
-        }
-
-        /* Note: Be careful if you override any DocumentWindow methods - the base
-           class uses a lot of them, so by overriding you might break its functionality.
-           It's best to do all your work in your content component instead, but if
-           you really have to override any DocumentWindow methods, make sure your
-           subclass also calls the superclass's method.
-        */
-
-    private:
-        JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainWindow)
-    };
+    void anotherInstanceStarted(const String& commandLine) override {}
 
 private:
     std::unique_ptr<MainWindow> mainWindow;
 };
 
-//==============================================================================
 // This macro generates the main() routine that launches the app.
-START_JUCE_APPLICATION (GuiAppTemplateApplication)
+START_JUCE_APPLICATION(GuiAppTemplateApplication)
--- Apps/GuiAppTemplate/Source/MainComponent.cpp
+++ Apps/GuiAppTemplate/Source/MainComponent.cpp
@@ -5,7 +5,6 @@ MainComponent::MainComponent()
     setSize (600, 400);
 }
 
-
 void MainComponent::paint (Graphics& g)
 {
     g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
