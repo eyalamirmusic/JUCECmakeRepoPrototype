3aab2b0fcc2977e799de1c3cae521a4860516de3
Eyal Amir <eyalamirmusic@gmail.com>
2020-04-22 16:20:34 +0300

    Switched back to add_subdirectory due to issues with custom modules. Removed demo runner from build.

--- CMake/FindJUCE-GIT.cmake
+++ CMake/FindJUCE-GIT.cmake
@@ -11,34 +11,25 @@ macro(update_juce_from_git)
             GIT_PROGRESS TRUE
             GIT_TAG "origin/juce6")
 
-    # Check whether we already set up JUCE during a previous configure
     FetchContent_GetProperties(juce)
 
     if (NOT juce_POPULATED)
         message("Updating JUCE from git...")
-        # Clone repo if necessary, record that JUCE has been populated
         FetchContent_Populate(juce)
-
         message("JUCE update finished")
     endif ()
 endmacro()
 
-function(install_juce_from_build_tree)
-    # Configure JUCE
-    execute_process(COMMAND "${CMAKE_COMMAND}" -S "${juce_SOURCE_DIR}" -B "${juce_BINARY_DIR}")
-
-    # Build JUCE
-    execute_process(COMMAND "${CMAKE_COMMAND}" --build "${juce_BINARY_DIR}")
-
-    include("${juce_BINARY_DIR}/JUCEExportConfig.cmake")
-endfunction()
-
-
 update_juce_from_git()
-install_juce_from_build_tree()
 
 #useful to build stuff like the projucer, audio plugin host, etc in your own project
 set(JUCE_SOURCE_ROOT ${CMAKE_CURRENT_BINARY_DIR}/_deps/juce-src)
+add_subdirectory(${JUCE_SOURCE_ROOT})
+
+
+
+
+
 
 
 
