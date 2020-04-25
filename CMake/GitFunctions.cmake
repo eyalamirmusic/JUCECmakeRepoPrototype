24185f817c1fdb40609d79797246ae5635d6156b
Eyal Amir <eyalamirmusic@gmail.com>
2020-04-25 23:52:20 +0300

    Cleaned up build scripts

--- CMake/GitFunctions.cmake
+++ CMake/GitFunctions.cmake
@@ -1,10 +1,7 @@
 include(FetchContent QUIET)
 
-#Helper file to grab JUCE automatically from git.
-#You can set this to use a specific commit, a git submodule, etc.
-
 #helper function to grab any repo from git:
-function (update_from_git name repo branch)
+macro (update_from_git name repo branch)
     FetchContent_Declare(${name}
             GIT_REPOSITORY ${repo}
             GIT_SHALLOW TRUE
@@ -13,19 +10,12 @@ function (update_from_git name repo branch)
 
     FetchContent_GetProperties(${name})
 
-    if (NOT juce_POPULATED)
+    if (NOT ${name}_POPULATED)
         message("Updating ${name} from git...")
         FetchContent_Populate(${name})
         message("${name} update finished")
     endif ()
-
-    set(${name}_SOURCE_ROOT ${CMAKE_CURRENT_BINARY_DIR}/_deps/${name}-src CACHE INTERNAL "")
-endfunction()
-
-function(add_subdirectory_from_git name repo branch)
-    update_from_git(${name} ${repo} ${branch})
-    add_subdirectory(${${name}_SOURCE_ROOT})
-endfunction()
+endmacro()
 
 
 
