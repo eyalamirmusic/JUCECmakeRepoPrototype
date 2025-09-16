8a25a4afb5648afbd8600f0180a1b1c277e2cfa4
Eyal Amir <eyalamirmusic@gmail.com>
2025-09-16 08:56:41 -0700

    Switching to cloning JUCE from a .zip archive instead of a git commit to speed up config

--- CMake/Findjuce.cmake
+++ CMake/Findjuce.cmake
@@ -14,4 +14,6 @@ option(JUCE_BUILD_EXAMPLES "Build JUCE Examples" OFF)
 #Fetching JUCE from git
 #IF you want to instead point it to a local version, you can invoke CMake with
 #-DCPM_JUCE_SOURCE="Path_To_JUCE"
-CPMAddPackage("gh:juce-framework/JUCE#develop")
+CPMAddPackage(NAME JUCE
+        URL
+        "https://github.com/juce-framework/JUCE/archive/refs/tags/8.0.10.zip")
